71d19fcf95a58dccd9c737ea54575df127e8e5f4
chien <minhchiendc@gmail.com>
2020-03-16 10:34:04 +0700

    Thêm Vet page và VetController

--- pet-clinic-web/src/test/java/guru/springframework/petclinic/PetClinicWebApplicationTests.java
+++ pet-clinic-web/src/test/java/guru/springframework/petclinic/PetClinicWebApplicationTests.java
@@ -1,10 +1,10 @@
-package sfgpetclinic;
+package guru.springframework.petclinic;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SfgPetClinicApplicationTests {
+class PetClinicWebApplicationTests {
 
     @Test
     void contextLoads() {
