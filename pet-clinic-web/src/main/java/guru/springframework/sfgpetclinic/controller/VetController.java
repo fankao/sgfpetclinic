6a340f6a0a2c8ed0af134991f98197c42995d13a
chien <minhchiendc@gmail.com>
2020-03-19 14:57:03 +0700

    Fix lỗi liên kết trang web

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/VetController.java
@@ -4,7 +4,6 @@ import guru.springframework.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-@RequestMapping("/vets")
 @Controller
 public class VetController {
     private final VetService vetService;
@@ -13,9 +12,10 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"","/index","/index.html"})
+    @RequestMapping({"/vets","/vets/index","/vets/index.html","/vets.html"})
     public String listVets(Model model){
         model.addAttribute("vets",vetService.findAll());
         return "vets/index";
     }
+
 }
